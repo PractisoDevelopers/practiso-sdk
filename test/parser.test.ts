0cb83db1e0d7963c327802d36ceb0b9b46d1e3e6
zhufucdev <zhufuzhufu1@gmail.com>
2025-06-24 21:38:56 +0800

    :white_check_mark: add image parsing test

--- test/parser.test.ts
+++ test/parser.test.ts
@@ -3,20 +3,30 @@ import { Parser } from '../src';
 import * as fs from 'node:fs';
 import { createReadableStreamFromReadable } from 'node-readable-stream';
 
-test('should parse sample archive', async function () {
+async function parseFile(file: string) {
     const parser = new Parser();
     await (
         createReadableStreamFromReadable(
-            fs.createReadStream('./test/sample.psarchive'),
+            fs.createReadStream(file),
         ) as ReadableStream
     )
         .pipeThrough(new DecompressionStream('gzip'))
         .pipeTo(parser.sink);
-    const archive = await parser.result();
+    return await parser.result();
+}
+
+test('should parse sample archive', async function () {
+    const archive = await parseFile('./test/sample.psarchive');
     const quizCount = 11;
     const framePerQuiz = 2;
     expect(archive.content.length).toBe(quizCount);
     expect(
         archive.content.reduce((acc, curr) => acc + curr.frames.length, 0),
     ).toBe(quizCount * framePerQuiz);
+});
+
+test('should parse image archive', async function () {
+    const archive = await parseFile('./test/sample.image.psarchive');
+    expect(archive.content.length).toBe(1);
+    expect(archive.resources.size).toBe(1);
 });
